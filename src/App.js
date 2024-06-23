2e82c93c2316127b13292b802ac1bf7776115f45
Ngoc <tc24sbytesizedhelper@gmail.com>
2024-06-23 18:30:42 +0200

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import LandingPage from "../src/components/LandingPage"; // Ensure correct path
+import LandingPage from "./components/LandingPage"; // Ensure correct path
 import Header from "./components/Header";
 function App() {
   return (
@@ -19,4 +19,3 @@ function App() {
 }
 
 export default App;
-git pu origin main
